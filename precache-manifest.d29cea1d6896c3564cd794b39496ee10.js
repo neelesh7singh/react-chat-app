c25f0726a2bf7803471586a4fe875a08474adf2b
neelesh7singh <neeleshsingh767@gmail.com>
2020-06-26 01:56:06 +0530

    Updates

---
+++ precache-manifest.d29cea1d6896c3564cd794b39496ee10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5270cda86c9323e35ca7ddd1ba1aff5d",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "eda13010a2c47d0a55f7",
+    "url": "/react-chat-app/static/css/main.f8fe5986.chunk.css"
+  },
+  {
+    "revision": "a39e85a86b205213d13f",
+    "url": "/react-chat-app/static/js/2.c9a75d96.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/react-chat-app/static/js/2.c9a75d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda13010a2c47d0a55f7",
+    "url": "/react-chat-app/static/js/main.41ab76b6.chunk.js"
+  },
+  {
+    "revision": "eb5d2dae52dbfedfad1d",
+    "url": "/react-chat-app/static/js/runtime-main.8f42f798.js"
+  }
+]);
